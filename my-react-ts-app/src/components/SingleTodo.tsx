abda59173a07408c6349d4440b985ecf59fbe6a6
Saroj Ekka <saroj.ekka@hpe.com>
2023-02-15 02:43:07 -0800

    from tutorial

--- my-react-ts-app/src/components/SingleTodo.tsx
+++ my-react-ts-app/src/components/SingleTodo.tsx
@@ -1,47 +1,48 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Todo } from "./model";
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
-import "./style.css";
+import { Todo } from "./model";
 
-interface Props {
+const SingleTodo: React.FC<{
   todo: Todo;
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-}
+}> = ({ todo, todos, setTodos }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
 
-const SingleTodo = ({ todo, todos, setTodos }: Props) => {
-  const [editFlag, seteditFlag] = useState<boolean>(false);
-  const [editTodo, seteditTodo] = useState<string>(todo.todo);
   const inputRef = useRef<HTMLInputElement>(null);
-
   useEffect(() => {
     inputRef.current?.focus();
-  }, [editFlag]);
-
-  const handleDone = (id: number) => {
-    setTodos(todos.map((t) => (t.id === id ? { ...t, isDone: !t.isDone } : t)));
-  };
-
-  const handleDelete = (id: number) => {
-    setTodos(todos.filter((t) => t.id !== id));
-  };
+  }, [edit]);
 
   const handleEdit = (e: React.FormEvent, id: number) => {
     e.preventDefault();
-    setTodos(todos.map((t) => (t.id === id ? { ...todo, t: editTodo } : t)));
-    seteditFlag(false);
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
   };
 
   return (
     <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
-      {editFlag ? (
+      {edit ? (
         <input
-          value={todo.todo}
-          onChange={(e) => {
-            console.log(e.target.value);
-            seteditTodo(e.target.value);
-          }}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
           className="todos__single--text"
           ref={inputRef}
         />
@@ -55,8 +56,8 @@ const SingleTodo = ({ todo, todos, setTodos }: Props) => {
         <span
           className="icon"
           onClick={() => {
-            if (!editFlag && !todo.isDone) {
-              seteditFlag(!editFlag);
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
             }
           }}
         >
