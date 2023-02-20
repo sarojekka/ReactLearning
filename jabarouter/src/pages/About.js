7e25cceb1e627e8b209965111984974ded8a987b
Saroj Ekka <saroj_ekka@yahoo.com>
2023-02-20 15:42:08 -0800

    last lesson

--- jabarouter/src/pages/About.js
+++ jabarouter/src/pages/About.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Navigate } from 'react-router-dom';
 
 function About() {
+  const [user, setUser] = useState('saroj');  
+  if (!user)
+  {
+    return <Navigate to="/" replace={true} />
+  }
+
   return (
     <div className="about">
       <h2>About Us</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum, maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.</p>
+
+      <button onClick={() => setUser(null)} >Logout</button>
     </div>
   )
 }
