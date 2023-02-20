b57816705eebeaea07c00313e5ba07bbb682a568
Saroj Ekka <saroj_ekka@yahoo.com>
2023-02-20 15:30:01 -0800

    learning router

--- jabarouter/src/App.js
+++ jabarouter/src/App.js
@@ -1,9 +1,49 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
+import CareersLayout from "./layouts/CareersLayout";
+import Faq from "./pages/help/Faq";
+import Contact, { contactAction } from "./pages/help/Contact";
+import NotFound from "./pages/NotFound";
+import Careers, { careersLoader } from "./pages/careers/Careers";
+import CareerDetails, { careerDetailsLoader } from "./pages/careers/CarreerDetails";
+import CareersError from "./pages/careers/CareersError";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />} >
+        <Route path="faq" element={<Faq />} / >
+        <Route path="Contact" element={<Contact />} action={contactAction} / >
+      </Route>
+      <Route path="careers" element={<CareersLayout />} errorElement={<CareersError />}>
+        <Route 
+          index 
+          element={<Careers />} 
+          loader={careersLoader}
+        />
+        <Route 
+          path=":id" 
+          element={<CareerDetails />} 
+          loader={careerDetailsLoader}
+        />
+      </Route>
+      <Route path="*" element={<NotFound />} / >
+    </Route>
+    
+  )
+);
 function App() {
-  return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
